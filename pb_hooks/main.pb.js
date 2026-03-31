f2c1179de1b4bdb18c898614ef614afab48f0f7c
lauraadan <info@lauraadan.com>
2026-03-31 10:49:39 +0200

    enable cors hooks

---
+++ pb_hooks/main.pb.js
@@ -0,0 +1,16 @@
+/// <reference path="../pb_data/types.d.ts" />
+
+routerUse((e) => {
+  e.response.headers.set("Access-Control-Allow-Origin", "*");
+  e.response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,OPTIONS",
+  );
+  e.response.headers.set("Access-Control-Allow-Headers", "*");
+
+  if (e.request.method === "OPTIONS") {
+    return e.json(200, {});
+  }
+
+  return e.next();
+});
